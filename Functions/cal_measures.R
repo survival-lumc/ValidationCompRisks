6fece36bf8de22eef96d2ff38f1f2f044cc75363
danielegiardiello <daniele.giardiello1@gmail.com>
2021-03-08 11:20:23 +0100

    Create cal_measures.R

---
+++ Functions/cal_measures.R
@@ -0,0 +1,63 @@
+# 8th March 2021
+# Goal: function to calculate calibration measures (ICI, E50, E90, Emax)
+# Author: Daniele Giardiello
+# Input
+# data: validation data
+# thorizon: time horizon for prediction
+# Tstop: observed time
+# status: variable status
+# cause: number indicating the event of interest. See ?riskRegression::predictRisk()
+
+cal_measures<-function(data,thorizon,fit,Tstop,status,cause) {
+  valid.df <- data
+  pred <- predictRisk(fit,cause=cause,newdata=data,times=thorizon)
+  cll.pred <- log(-log(1-pred))
+  
+  # Create grid along which to create calibration curves.
+  range.pred <- quantile(pred,probs=c(0.01,0.99))
+  pred.grid<- seq(from=range.pred[1],to=range.pred[2],length=100)
+  cll.pred.grid <- log(-log(1-pred.grid))
+  
+  remove(range.pred)
+  
+  # Create RCS components using 3 knots.
+  rcs3.mat <- rcspline.eval(cll.pred,nk=3,inclx=T)
+  knots3 <- attr(rcs3.mat,"knots")
+  valid.df$rcs3.x1 <- rcs3.mat[,1]
+  valid.df$rcs3.x2 <- rcs3.mat[,2]
+  
+  # Create RCS terms for grid along which predictions will be obtained.
+  # Use the CLL transformation of the grid. Use the knots defined above.
+  grid3.rcs <- rcspline.eval(cll.pred.grid,knots=knots3,inclx=T)
+  rcs3.x1 <- grid3.rcs[,1]
+  rcs3.x2 <- grid3.rcs[,2]
+  rcs3.df <- data.frame(cbind(rcs3.x1,rcs3.x2))
+  
+  remove(rcs3.mat,knots3,grid3.rcs,rcs3.x1,rcs3.x2)
+  
+  # Fit F-G model to predicted probabilities using RCS.
+  # Obtain estimated probabilities from fitted model at the grid points.
+  # This is for the calibration curve.
+  
+  # [NOTE] Expand validation data using mstate::crprep()
+  valid.df.w<-crprep(Tstop=Tstop,status=status,trans=cause,
+                     keep=c('rcs3.x1','rcs3.x2'),data=valid.df)
+  
+  model.calibrate.fg <- cph(Surv(Tstart,Tstop,status==1)~rcs3.x1+rcs3.x2,
+                            weights=weight.cens,x=T,y=T,surv=T,data=valid.df.w)
+  
+  #obs.grid.fg <- 1-survest(model.calibrate.fg,newdata=rcs3.df,time=5)$surv
+  ## [NOTE] save the confidence levels of the grid useful for the calibration plot confidence bands
+  #obs.grid.fg.upper<-1-survest(model.calibrate.fg,newdata=rcs3.df,time=5)$lower
+  #obs.grid.fg.lower<-1-survest(model.calibrate.fg,newdata=rcs3.df,time=yr*365)$upper
+  
+  obs.fg <- 1-survest(model.calibrate.fg,newdata=valid.df,time=5)$surv
+  ici.fg <- mean(abs(pred - obs.fg))
+  E50.fg <- median(abs(pred - obs.fg))
+  E90.fg <- quantile(abs(pred - obs.fg),probs=0.90)
+  Emax.fg <-max(abs(pred - obs.fg))
+  
+  res_calmeas<-c(ici.fg,E50.fg,E90.fg,Emax.fg)
+  names(res_calmeas)<-c('ICI','E50','E90','Emax')
+  return(res_calmeas)
+}
