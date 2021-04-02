ed73797ae76151ad190af784572fa5caf4b0d939
efbonneville <e.f.bonneville@lumc.nl>
2021-04-02 10:53:30 +0200

    Added template documentation for datasets, and removed redundant Data/Readme.md file

---
+++ R/datasets.R
@@ -0,0 +1,23 @@
+#' Development dataset
+#' 
+#' Short description of data.
+#' 
+#' @format A data frame with x rows and y variables
+#' \describe{
+#'  \item{Variable 1 name}{Variable 1 description}
+#'  \item{Variable 2 name}{Variable 2 description}
+#' }
+#' 
+NULL
+
+#' Validation dataset
+#' 
+#' Short description of data.
+#' 
+#' @format A data frame with x rows and y variables
+#' \describe{
+#'  \item{Variable 1 name}{Variable 1 description}
+#'  \item{Variable 2 name}{Variable 2 description}
+#' }
+#' 
+NULL
